cb9c9a81e25528091b9b276507cdea8ea1416755
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2020-04-17 15:18:12 -0600

    Specify path for File and Directory inputs and expand examples

--- examples/run.sh
+++ examples/run.sh
@@ -2,11 +2,12 @@
 
 set -o errexit
 
-#cwl-runner --outdir Output_hello1 01_hello.cwl hello_job.yml
-#cwl-runner --outdir Output_hello2 02_hello.cwl hello_job.yml
-#cwl-runner --outdir Output_hello3 03_hello.cwl hello_job.yml
+#cwl-runner --outdir Output/01_hello 01_hello.cwl hello_job.yml
+#cwl-runner --outdir Output/02_hello 02_hello.cwl hello_job.yml
+#cwl-runner --outdir Output/03_hello 03_hello.cwl hello_job.yml
 
-#cwl-runner --outdir Output_BMI bmi_calculator.cwl bmi_job.yml
-cwl-runner --outdir Output_BMI_Ave bmi_calculator_average.cwl bmi_average_job.yml
+#cwl-runner --outdir Output/BMI bmi_calculator.cwl bmi_calculator_job.yml
+cwl-runner --outdir Output/BMI_url bmi_calculator_url.cwl bmi_calculator_url_job.yml
+#cwl-runner --outdir Output/BMI_Ave bmi_calculator_average.cwl bmi_average_job.yml
 
-#cwl-runner --outdir Output_DESeq2 deseq2.cwl deseq2_job.yml
+#cwl-runner --outdir Output/DESeq2 deseq2.cwl deseq2_job.yml
